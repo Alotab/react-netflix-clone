679e8033dcf0c6aba7d00ded346a601d0b88501d
Albert Osei Frimpong <Alotab@yahoo.com>
2022-11-30 00:00:55 +0000

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYw4FxS9kJsFhOolDh3HeByP2zSgyj_Sg",
+    authDomain: "react-netflix-clone-462d9.firebaseapp.com",
+    projectId: "react-netflix-clone-462d9",
+    storageBucket: "react-netflix-clone-462d9.appspot.com",
+    messagingSenderId: "308358963100",
+    appId: "1:308358963100:web:9dfc6b1936a478d97c3259"
+  };
