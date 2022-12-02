799c7a546c96c6851a1f2a37261442459990a5d2
Albert Osei Frimpong <Alotab@yahoo.com>
2022-12-02 14:35:11 +0000

    update

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import axios from "./axios";
 import "./Row.css";
 
-const Row = () => {
+const Row = ({title, fetchUrl, islargeRow = false }) => {
+
+    const [movies, setMovies ] = useState([]);
+
+    const base_url = "https://image.tmdb.org/t/p/original/"
+
+    useEffect(() => {
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+
+    }, [fetchUrl]);
+
+
   return (
-    <div className="row">Row
-    
-    
+    <div className="row">
+        <h2>{title}</h2>
+
+        <div className="row_posters">
+        {movies.map((movie)  => (
+            <img 
+                className={`row_poster ${islargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                islargeRow ? movie.poster_path : movie.backdrop_path
+            }`} alt={movie.name}
+            />
+        ))}
+        </div>
     </div>
   )
 }
 
-export default Row
+export default Row;
