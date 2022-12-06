d434f9b4643ef0d38c97620877c984554a5d7f30
Albert Osei Frimpong <Alotab@yahoo.com>
2022-12-06 18:44:01 +0000

    redux update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from './screens/LoginScreen';
-
 import './App.css';
 import {BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice';
 
 
 
 function App() {
-
   const user = null;
+  //const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+        }))
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="App">
